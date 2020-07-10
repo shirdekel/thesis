441a2b2ec4c2e372dc501b41c4e1ccc525b688ee
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-10 16:31:05 +1000

    Add restriction values function
    - Get expected value and gain/loss ratio

---
+++ R/get_restriction_values.R
@@ -0,0 +1,24 @@
+##' @title Get restriction values (EV and gain/loss)
+##' @param prob_positive_sample
+##' @param outcome_positive_sample
+##' @param outcome_dif
+##' @return
+##' @author Shir Dekel
+##' @export
+get_restriction_values <- function(prob_positive_sample,
+                                   outcome_positive_sample, outcome_dif) {
+
+  expected_value_positive <- prob_positive_sample * outcome_positive_sample
+  prob_negative_sample <- 1 - prob_positive_sample
+  outcome_negative_sample <- outcome_dif - outcome_positive_sample
+  expected_value_negative <- prob_negative_sample * outcome_negative_sample
+
+  expected_value <- expected_value_positive - expected_value_negative
+  gain_loss_ratio <- expected_value_positive / expected_value_negative
+
+  restriction_values <- list(expected_value = expected_value,
+                             gain_loss_ratio = gain_loss_ratio)
+
+  return(restriction_values)
+
+}
