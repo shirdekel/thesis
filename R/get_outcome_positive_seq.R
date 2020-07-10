fd1a14a3fedfaf0f48e60c2af3530c2dd9871c1a
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-10 16:30:11 +1000

    Add positive outcome sequence function

---
+++ R/get_outcome_positive_seq.R
@@ -0,0 +1,13 @@
+##' @title Generate positive outcome sequence
+
+##' @return
+##' @author Shir Dekel
+##' @export
+get_outcome_positive_seq <- function() {
+
+
+  outcome_positive_seq <- seq(from = 100, to = 200, by = 10)
+
+  return(outcome_positive_seq)
+
+}
