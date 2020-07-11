78ac06758451f0ae2a20ba6fbf9e3cd1a2f871b8
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-11 17:08:49 +1000

    Fix figure colour order
    - So that it works even if there is no zero outcome

--- R/plot_gamble.R
+++ R/plot_gamble.R
@@ -6,18 +6,27 @@
 ##' @export
 plot_gamble <- function(gambles) {
 
+  # Determine whether the figure will have zero or not
+  # If zero exists, the order of colorspace fill needs to be specified so that zero gets the blue colour
+  if(0 %in% gambles$outcome_aggregated) {
+    colour_order <- c(1, 3, 2)
+  } else {
+    colour_order <- c(1, 2)
+  }
+
   gamble_plot <- tibble(outcomes = gambles$outcome_aggregated,
          probs = gambles$prob_aggregated,
          colour_group = case_when(
            outcomes > 0 ~ "Positive",
            outcomes < 0 ~ "Negative",
-           TRUE ~ "Zero"
-         )) %>%
+           outcomes == 0 ~ "Zero"
+         ) %>%
+           fct_relevel("Positive", after = 2)) %>%
     ggplot(aes(x = outcomes,
                y = probs,
                fill = colour_group)) +
     geom_col() +
-    scale_fill_discrete_qualitative("Dark 3") +
+    scale_fill_discrete_qualitative("Dark 3", order = colour_order) +
     labs(x = "Outcomes ($ million)",
          y = "Probability",
          fill = "Outcome type") +
