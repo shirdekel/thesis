a49b405a3b89daaf10f37076e0586a6b84d909bf
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-11 14:38:08 +1000

    Add gain/loss ratio restriction argument

--- R/get_restriction.R
+++ R/get_restriction.R
@@ -1,11 +1,14 @@
 ##' @title Get restriction logicals
+##'
+##' @param gain_loss_ratio_restriction
 ##' @param restriction_values
+##'
 ##' @return
 ##' @author Shir Dekel
 ##' @export
-get_restriction <- function(restriction_values) {
+get_restriction <- function(restriction_values, gain_loss_ratio_restriction) {
 
-  restriction <- (restriction_values$expected_value > 0) & (restriction_values$gain_loss_ratio < 2)
+  restriction <- (restriction_values$expected_value > 0) & (restriction_values$gain_loss_ratio < gain_loss_ratio_restriction)
 
   return(restriction)
 
