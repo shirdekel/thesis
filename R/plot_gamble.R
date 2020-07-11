f5e1f30e6d7153c2cbd80ab1f896ef64a548e6dd
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-11 15:08:46 +1000

    Remove assumption of three levels
    - Some times the distribution does not have a zero value

--- R/plot_gamble.R
+++ R/plot_gamble.R
@@ -11,15 +11,13 @@ plot_gamble <- function(gambles) {
          colour_group = case_when(
            outcomes > 0 ~ "Positive",
            outcomes < 0 ~ "Negative",
-           outcomes == 0 ~ "Zero"
-         ) %>%
-           fct_relevel("Positive", "Zero", "Negative")) %>%
+           TRUE ~ "Zero"
+         )) %>%
     ggplot(aes(x = outcomes,
                y = probs,
                fill = colour_group)) +
     geom_col() +
-    scale_fill_discrete_qualitative("Dark 3",
-                                    order = c(2, 3, 1)) +
+    scale_fill_discrete_qualitative("Dark 3") +
     labs(x = "Outcomes ($ million)",
          y = "Probability",
          fill = "Outcome type") +
