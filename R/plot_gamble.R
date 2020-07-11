a2bd4880fbb7599c0e44b583d271e8ddb48670b9
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-11 14:38:50 +1000

    Add plot detail

--- R/plot_gamble.R
+++ R/plot_gamble.R
@@ -6,9 +6,25 @@
 ##' @export
 plot_gamble <- function(gambles) {
 
-  tibble(outcomes = gambles$outcome_aggregated,
-         probs = gambles$prob_aggregated) %>%
-    ggplot(aes(outcomes, probs)) +
-    geom_col()
+  gamble_plot <- tibble(outcomes = gambles$outcome_aggregated,
+         probs = gambles$prob_aggregated,
+         colour_group = case_when(
+           outcomes > 0 ~ "Positive",
+           outcomes < 0 ~ "Negative",
+           outcomes == 0 ~ "Zero"
+         ) %>%
+           fct_relevel("Positive", "Zero", "Negative")) %>%
+    ggplot(aes(x = outcomes,
+               y = probs,
+               fill = colour_group)) +
+    geom_col() +
+    scale_fill_discrete_qualitative("Dark 3",
+                                    order = c(2, 3, 1)) +
+    labs(x = "Outcomes ($ million)",
+         y = "Probability",
+         fill = "Outcome type") +
+    scale_x_continuous(breaks = gambles$outcome_aggregated)
+
+  return(gamble_plot)
 
 }
