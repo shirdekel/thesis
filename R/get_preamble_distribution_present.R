a93b320e7108a9eded3e7b925c8375904dc6c960
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-14 21:18:17 +1000

    Add distribution explanation

--- R/get_preamble_distribution_present.R
+++ R/get_preamble_distribution_present.R
@@ -7,7 +7,9 @@ get_preamble_distribution_present <- function(preamble_distribution_absent) {
 
   preamble_distribution_present <- preamble_distribution_absent %>%
     str_c(
-      p("Below is the probability distribution of final outcomes if all gambles were chosen.") %>%
+      div(
+        p("Below is the probability distribution of final outcomes if all gambles were chosen."),
+        p("The numbers on the x-axis (labelled 'Outcome') represent the final amounts of money possible if you chose to invest in all the projects. The numbers on the y-axis (labelled 'Probability') represent the likelihoods of each of the possible outcomes. Negative final outcomes (losses) are shown in red, positive final outcomes (gains) are shown in green, and a final outcome of zero (no loss or gain) is shown in blue.")) %>%
         as.character(),
       div(
         img(src = insert_resource("distribution.png"),
