472d88e020f840a7eba5ce34766005f61cea2f59
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-14 21:18:00 +1000

    Add exit prompt and consent code

--- R/get_experiment.R
+++ R/get_experiment.R
@@ -19,11 +19,28 @@ get_experiment <- function(...) {
     vanilla = c(
       "awareness_presentation = jsPsych.randomization.sampleWithoutReplacement(['aware_separate', 'naive_joint', 'naive_separate'], 1)[0]",
       "regex_awareness = /.*(?=_)/",
-      "regex_presentation = /(?<=_).*/"
-    ),
+      "regex_presentation = /(?<=_).*/") %>%
+    coffee_compile(bare = T) %>%
+      c("var check_consent = function(elem) {
+  if (document.getElementById('consent_checkbox').checked) {
+    return true;
+  }
+  else {
+    alert('If you wish to participate, you must check the box next to the statement <em>I agree to participate in this study.</em>');
+    return false;
+  }
+  return false;
+};"),
     path = here("inst", "jspsych"),
+    on_close = insert_javascript(" function(){
+    event.preventDefault();
+    event.returnValue = '';
+  }"),
     on_finish = save_locally()
   )
 
   return(experiment)
 }
+
+
+
