0674d352aa4d4632aad4350b554fa29bba55de7d
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-16 20:00:47 +1000

    Change preamble wording
    - "Gamble" to "project"

--- R/get_preamble_distribution_present.R
+++ R/get_preamble_distribution_present.R
@@ -7,7 +7,7 @@ get_preamble_distribution_present <- function(preamble_distribution_absent) {
 
   preamble_distribution_present <- str_c(
       div(
-        p("Below is the probability distribution of final outcomes if all gambles were chosen."),
+        p("Below is the probability distribution of final outcomes if all projects were chosen."),
         p("The numbers on the x-axis (labelled 'Outcome') represent the final amounts of money possible if you chose to invest in all the projects. The numbers on the y-axis (labelled 'Probability') represent the likelihoods of each of the possible outcomes. Negative final outcomes (losses) are shown in red, positive final outcomes (gains) are shown in green, and a final outcome of zero (no loss or gain) is shown in blue.")) %>%
         as.character(),
       div(
