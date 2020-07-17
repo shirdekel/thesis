f5311b4705ca9b191965f5ccc266b6493256ef1d
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-17 10:21:37 +1000

    Add debrief

---
+++ R/get_debrief.R
@@ -0,0 +1,22 @@
+##' @title Get debrief
+
+##' @return
+##' @author Shir Dekel
+##' @export
+get_debrief <- function() {
+
+  debrief_html <- str_c("debrief", 1:2, ".png") %>%
+    map_chr(~ img(src = insert_resource(.x),
+                  width = "750") %>%
+              as.character())
+
+  debrief <- trial_instructions(
+    pages = c(
+      debrief_html
+    ),
+    show_clickable_nav = TRUE
+  )
+
+  return(debrief)
+
+}
