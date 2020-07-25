5029b6dd0ea5f40311971200be3051a971ae3373
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-25 12:01:19 +1000

    Combine experiment_pre functions

---
+++ R/get_experiment_pre.R
@@ -0,0 +1,25 @@
+##' @title Get pre experiment trials
+
+##' @return
+##' @author Shir Dekel
+##' @export
+get_experiment_pre <- function() {
+
+  pis <- get_pis("prolific")
+  get_consent_html()
+  consent <- get_consent()
+  sample_id <- get_sample_id("prolific")
+  contact <- get_contact()
+  demographics <- get_demographics()
+  business_information <- get_business_information()
+
+  experiment_pre <- build_timeline(pis,
+                                   consent,
+                                   sample_id,
+                                   contact,
+                                   demographics,
+                                   business_information)
+
+  return(experiment_pre)
+
+}
