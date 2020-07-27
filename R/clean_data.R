6a942383b76dd504164dc1afe8ff735aecd7dbe5
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-27 21:39:00 +1000

    Update clean_data()
    - Parse non-project-choice responses and join to project-choice data

--- R/clean_data.R
+++ R/clean_data.R
@@ -7,66 +7,68 @@
 ##' @export
 clean_data <- function(data_raw) {
 
-  data_raw_na_drop <- data_raw %>%
-    drop_na(responses)
+  data_raw_prep <- data_raw %>%
+    drop_na(experiment, stage) %>% # Filter out old data, but only taking rows with experiment and stage values
+    rowwise() %>%
+    mutate(across(c(experiment, sample, stage), . %>%
+             fromJSON())) %>%
+    filter(experiment == "aggregation_exp2")
+
+ data_other <- data_raw_prep %>%
+   drop_na(responses) %>%
+    filter(stage != "project_choice") %>%
+    nest_by(subject) %>%
+    mutate(other = data %>%
+             pull(responses) %>%
+             map_dfc(fromJSON) %>%
+             list()) %>%
+   unnest(other) %>%
+   select(-data)
 
   names_to <- c("project", "outcome_positive", "outcome_dif", "probability_positive")
   values_to <- "choice"
 
-  extract_responses <- function(data) {
-    data %>%
-      pivot_longer(cols = -(file_id:question_order),
-                   names_to = names_to,
-                   names_sep = "_",
-                   values_to = values_to)
-  }
+  data_separate <- data_raw_prep %>%
+    filter(stage == "project_choice", presentation == "separate") %>%
+    group_by(subject) %>%
+    mutate(project_order = 1:10) %>%
+    rowwise() %>%
+    mutate(responses %>%
+             map_dfc(fromJSON) %>% # Doesn't work as a single step without pivot_longer()
+             pivot_longer(cols = everything(),
+                          names_to = names_to,
+                          names_sep = "_",
+                          values_to = values_to)
+    )
 
-  data_joint <- data_raw_na_drop %>%
-    filter(presentation == "joint") %>%
+  data_joint <- data_raw_prep %>%
+    filter(stage == "project_choice", presentation == "joint") %>%
     rowwise()  %>%
     mutate(responses %>%
-             map_dfc(fromJSON)) %>%
-    extract_responses() %>%
-    nest_by(file_id, question_order) %>%
-    mutate(question_order = question_order %>%
+             map_dfc(fromJSON))%>%
+    pivot_longer(cols = -(trial_type:stage),
+                 names_to = names_to,
+                 names_sep = "_",
+                 values_to = values_to) %>%
+    nest_by(subject, question_order) %>%
+    mutate(project_order = question_order %>%
              map(fromJSON)) %>%
-    unnest(c(data, question_order)) %>%
-    mutate(question_order = question_order + 1) %>%
+    unnest(c(data, project_order)) %>%
+    mutate(project_order = project_order + 1) %>%
     ungroup()
-
-  get_separate_responses <- function(data) {
-    data %>%
-      pull(responses) %>%
-      map_dfc(fromJSON) %>%
-      pivot_longer(cols = everything(),
-                   names_to = names_to,
-                   names_sep = "_",
-                   values_to = values_to)
-  }
-
-  data_separate <- data_raw_na_drop %>%
-    filter(presentation == "separate") %>%
-    group_by(file_id) %>%
-    mutate(question_order = 1:10) %>%
-    nest_by() %>%
-    mutate(responses = get_separate_responses(data) %>%
-             list()) %>%
-    unnest(c(data, responses))
 
   data <- data_joint %>%
     bind_rows(data_separate) %>%
-    nest_by(file_id) %>%
-    mutate(total_time = shiR::gettime(data)) %>%
-    unnest(data) %>%
-    ungroup() %>%
-    separate(file_id, into = c(NA, "datetime", "id"), sep = "_") %>%
+    group_by(subject) %>%
     mutate(choice = recode(choice, "Yes" = 1, "No" = 0),
-           datetime = datetime %>%
-             as_datetime(tz = "Australia/Sydney")) %>%
-    select(-c(rt:internal_node_id, responses)) %>%
-    select(-where(~.x %>%
-                    is.na() %>%
-                    all()))
+           datetime = dateCreated %>%
+             dmy_hms(tz = "Australia/Sydney"),
+           total_time = max(time_elapsed),
+           proportion = sum(choice)/10) %>%
+    ungroup() %>%
+    select(subject, project_order, experiment:presentation, stage:proportion) %>%
+    inner_join(data_other, by = "subject") %>%
+    filter(!str_detect(prolific, "test1234"))
 
   return(data)
 
