35093fd48dd0443daf8b39dc79b6ef576461d270
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-27 12:03:26 +1000

    Add condition_allocation()
    - Vanilla JS to add for random condition allocation
    
    - Moved from get_experiment()

---
+++ R/condition_allocation.R
@@ -0,0 +1,14 @@
+##' @title Pase condition allocation JS code
+
+##' @return
+##' @author Shir Dekel
+##' @export
+condition_allocation <- function() {
+
+  c("condition = jsPsych.randomization.sampleWithoutReplacement(['naive_joint_absent', 'naive_separate_absent', 'naive_separate_present'], 1)[0]",
+    "regex_awareness = /(.*)_.*_.*/",
+    "regex_presentation = /.*_(.*)_.*/",
+    "regex_distribution = /.*_.*_(.*)/") %>%
+    coffee_compile(bare = TRUE)
+
+}
