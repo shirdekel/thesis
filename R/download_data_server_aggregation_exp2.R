73a654e2678cc02494dcd868f38818a4314138f1
Shir Dekel <shirdekel@yahoo.com.au>
2020-07-28 14:02:57 +1000

    Add data download wrapper

---
+++ R/download_data_server_aggregation_exp2.R
@@ -0,0 +1,10 @@
+##' @title Download server data
+
+##' @return
+##' @author Shir Dekel
+##' @export
+download_data_server_aggregation_exp2 <- function() {
+
+  download_data_server(here("inst", "extdata", "psychsydexp"))
+
+}
