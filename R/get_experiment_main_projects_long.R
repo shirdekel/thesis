b052c9bc8876a0e746fc3ec62d25f016f43ef358
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-19 11:25:24 +1000

    Consolidate long project trial generation into get_trial_distribution_absent()

--- R/get_experiment_main_projects_long.R
+++ R/get_experiment_main_projects_long.R
@@ -18,76 +18,10 @@ get_experiment_main_projects_long <- function(projects_long) {
     p("Indicate below whether you would invest in the following:") %>%
     as.character()
 
-  form_options <-
-    c("Yes", "No")
-
-  questions_joint <-
-    projects_long %>%
-    pmap(
-      ~ list(.x, .y) %>%
-        pmap(~
-        get_questions_joint(.x,
-                            form_options,
-                            .y)
-        )
-    )
-
-  similarity_condition <-
-    projects_long$description %>%
-    names() %>%
-    str_match(".*_(.*)") %>%
-    .[, 2]
-
-  project_variation <-
-    1:length(projects_long$description$similarity_low) %>%
-    as.numeric()
-
-  trial_joint_distribution_absent <-
-    list(
-      questions_joint,
-      similarity_condition
-    ) %>%
-    pmap(
-      function(questions_variations, similarity_condition)
-        list(questions_variations,
-             project_variation) %>%
-        pmap(
-          function(questions, project_variation) get_trial_joint(preamble_distribution_absent,
-                                                                 questions,
-                            "absent") %>%
-            build_timeline() %>%
-            display_if(fn_data_condition(similarity == !!similarity_condition)) %>%
-            build_timeline() %>%
-            display_if(fn_data_condition(project_variation == !!project_variation))
-        )
-    )
-
-  trial_separate_distribution_absent <-
-    projects_long %>%
-    append(
-      list(similarity_condition)
-    ) %>%
-    pmap(
-      function(description_variations, input_variations, similarity_condition)
-        list(description_variations,
-             input_variations,
-             project_variation) %>%
-        pmap(
-          function(description,
-                   input,
-                   project_variation)
-               get_trial_separate(
-                 preamble_distribution_absent,
-                 description,
-                 form_options,
-                 input,
-                 "absent"
-               ) %>%
-               build_timeline() %>%
-               display_if(fn_data_condition(similarity == !!similarity_condition)) %>%
-               build_timeline() %>%
-               display_if(fn_data_condition(project_variation == !!project_variation))
-        )
+  trial_distribution_absent <-
+    get_trial_distribution_absent(
+      projects_long,
+      preamble_distribution_absent
     )
 
   experiment_main_projects_long <-
@@ -97,16 +31,16 @@ get_experiment_main_projects_long <- function(projects_long) {
     ) %>%
     append(
       list(
-        trial_joint_distribution_absent$similarity_low,
-        trial_joint_distribution_absent$similarity_high,
-        trial_separate_distribution_absent$similarity_low,
-        trial_separate_distribution_absent$similarity_high
+        trial_distribution_absent$joint$similarity_low,
+        trial_distribution_absent$joint$similarity_high,
+        trial_distribution_absent$separate$similarity_low,
+        trial_distribution_absent$separate$similarity_high
       ) %>%
         flatten()
     ) %>%
     build_timeline() %>%
     flatten()
 
-      return(experiment_main_projects_long)
+  return(experiment_main_projects_long)
 
 }
---
+++ R/get_trial_distribution_absent.R
@@ -0,0 +1,89 @@
+##' @title Get long projects distribution absent trial
+##'
+##' @param projects_long
+##' @param preamble_distribution_absent
+##'
+##' @return
+##' @author Shir Dekel
+##' @export
+get_trial_distribution_absent <- function(projects_long,
+                                          preamble_distribution_absent) {
+
+  similarity_condition <-
+    projects_long$description %>%
+    names() %>%
+    str_match(".*_(.*)") %>%
+    .[, 2]
+
+  project_variation <-
+    1:length(projects_long$description$similarity_low) %>%
+    as.numeric()
+
+  questions_joint <-
+    projects_long %>%
+    pmap(
+      ~ list(.x, .y) %>%
+        pmap(~
+               get_questions_joint(project_description = .x,
+                                   project_input = .y)
+        )
+    )
+
+  trial_joint_distribution_absent <-
+    list(
+      questions_joint,
+      similarity_condition
+    ) %>%
+    pmap(
+      function(questions_variations, similarity_condition)
+        list(questions_variations,
+             project_variation) %>%
+        pmap(
+          function(questions, project_variation) get_trial_joint(
+            preamble_distribution_absent,
+            questions,
+            "absent"
+          ) %>%
+            build_timeline() %>%
+            display_if(fn_data_condition(similarity == !!similarity_condition)) %>%
+            build_timeline() %>%
+            display_if(fn_data_condition(project_variation == !!project_variation))
+        )
+    )
+
+  trial_separate_distribution_absent <-
+    projects_long %>%
+    append(
+      list(similarity_condition)
+    ) %>%
+    pmap(
+      function(description_variations, input_variations, similarity_condition)
+        list(description_variations,
+             input_variations,
+             project_variation) %>%
+        pmap(
+          function(description,
+                   input,
+                   project_variation)
+            get_trial_separate(
+              preamble = preamble_distribution_absent,
+              project_description = description,
+              project_input = input,
+              distribution = "absent"
+            ) %>%
+            build_timeline() %>%
+            display_if(fn_data_condition(similarity == !!similarity_condition)) %>%
+            build_timeline() %>%
+            display_if(fn_data_condition(project_variation == !!project_variation))
+        )
+    )
+
+  trial_distribution_absent <-
+    list(
+      joint = trial_joint_distribution_absent,
+      separate = trial_separate_distribution_absent
+    )
+
+  return(trial_distribution_absent)
+
+}
