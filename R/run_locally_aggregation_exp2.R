4923abe94f9712155fe3ef84c9e9ac91a89c1aa3
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-20 14:10:32 +1000

    Add project-specific run_locally() wrapper

---
+++ R/run_locally_aggregation_exp2.R
@@ -0,0 +1,11 @@
+##' @title Run locally
+
+##' @return
+##' @author Shir Dekel
+##' @export
+run_locally_aggregation_exp2 <- function() {
+
+  here("inst", "jspsych") %>%
+         run_locally()
+
+}
