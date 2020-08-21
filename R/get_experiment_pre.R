b1ce8e1e8e3acae9e239c790c433bd11996950fc
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-21 14:02:51 +1000

    Clean up pre-experiment function

--- R/get_experiment_pre.R
+++ R/get_experiment_pre.R
@@ -5,20 +5,35 @@
 ##' @export
 get_experiment_pre <- function() {
 
-  pis <- get_pis("prolific")
-  get_consent_html()
-  consent <- get_consent()
-  sample_id <- get_sample_id("prolific")
-  contact <- get_contact()
-  demographics <- get_demographics()
-  business_information <- get_business_information()
+  pis <-
+    get_pis("prolific")
 
-  experiment_pre <- build_timeline(pis,
-                                   consent,
-                                   sample_id,
-                                   contact,
-                                   demographics,
-                                   business_information)
+  get_consent_html()
+
+  consent <-
+    get_consent()
+
+  sample_id <-
+    get_sample_id("prolific")
+
+  contact <-
+    get_contact()
+
+  demographics <-
+    get_demographics()
+
+  business_information <-
+    get_business_information()
+
+  experiment_pre <-
+    build_timeline(
+      pis,
+      consent,
+      sample_id,
+      contact,
+      demographics,
+      business_information
+    )
 
   return(experiment_pre)
 
