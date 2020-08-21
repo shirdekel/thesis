9075b6cb5d5599810108ab96639012d8f284013f
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-21 13:58:09 +1000

    Update Experiment 2 condition allocation

---
+++ R/condition_allocation_experiment2.R
@@ -0,0 +1,29 @@
+##' @title Parse condition allocation JS code for E2
+
+##' @return
+##' @author Shir Dekel
+##' @export
+condition_allocation_experiment2 <- function() {
+
+  c("naive_separate_absent",
+    "naive_joint_absent",
+    "naive_separate_present",
+    "aware_separate_absent") %>%
+    toJSON() %>%
+    str_c(
+      "condition = jsPsych.randomization.sampleWithoutReplacement(",
+      .,
+      ", 1)[0]"
+    ) %>%
+    c(
+      "regex_awareness = /(.*)_.*_.*/",
+      "regex_presentation = /.*_(.*)_.*/",
+      "regex_distribution = /.*_.*_(.*)/",
+      # Add [1] to extract capture group
+      "awareness_condition = condition.match(regex_awareness)[1]",
+      "presentation_condition = condition.match(regex_presentation)[1]",
+      "distribution_condition = condition.match(regex_distribution)[1]") %>%
+    coffee_compile(bare = TRUE)
+
+
+}
