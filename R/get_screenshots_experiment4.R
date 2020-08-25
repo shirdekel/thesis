91a80b594aef34ff57f90378f64a7d93096a3509
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-25 18:42:01 +1000

    Add interstitial screenshots

--- R/get_screenshots_experiment4.R
+++ R/get_screenshots_experiment4.R
@@ -37,4 +37,36 @@ get_screenshots_experiment4 <- function() {
         )
     )
 
+  c("naive", "aware") %>%
+    map(
+      ~ file.path(file_paths_experiment4, "index.html") %>%
+        webshot(
+          file = file.path(
+            file_paths_materials_experiment4,
+            str_c("interstitial_", .x, ".png")
+          ),
+          selector = "#jspsych-content",
+          eval =
+            str_c(
+              "casper.thenOpen(this.getCurrentUrl() + '?awareness=",
+              .x,
+              "', function() {
+
+              // Test page
+              this.click('#jspsych-html-button-response-button-0');
+
+              // Instructions 1
+              this.click('#jspsych-instructions-next');
+
+              // Instructions 2
+              this.click('#jspsych-instructions-next');
+
+              // Instructions 3
+              this.click('#jspsych-instructions-next');
+
+    });"
+            )
+        )
+    )
+
 }
