b2f12e43140f09c21f625eb68db1fdb8948032db
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-25 18:42:40 +1000

    Update Experiment 4 trials
    - Move interstitials to preamble

--- R/get_trial_separate_experiment4.R
+++ R/get_trial_separate_experiment4.R
@@ -9,50 +9,65 @@
 ##' @return
 ##' @author Shir Dekel
 ##' @export
-get_trial_separate_experiment4 <- function(preamble = "<p>Indicate below whether you would invest in the following:</p>", timeline_variable, form_options = c("Yes", "No"), awareness, project_variation_value ="") {
+get_trial_separate_experiment4 <- function(preamble, timeline_variable, form_options = c("Yes", "No"), awareness, project_variation_value) {
+
+  preamble <-
+    p(
+      "Indicate below whether you would invest in the project:"
+    )
+
+  if (awareness == "naive") {
+    preamble_awareness <-
+      div(
+        p(
+          "Below is a description of one project."
+        ),
+        preamble
+      ) %>%
+      as.character()
+  } else if (awareness == "aware") {
+    preamble_awareness <-
+      div(
+        p(
+          "Below is a description of project ' +
+        jsPsych.data.getLastTrialData().select('current_project_choice_order').values[0] +
+        ' of 20.",
+          .noWS = "after"
+        ),
+        preamble,
+        .noWS = "inside"
+      ) %>%
+      as.character() %>%
+      str_c(
+        "function() {
+        preamble_awareness = '", .,"'
+        return preamble_awareness
+        }"
+      ) %>%
+      insert_javascript()
+  }
 
   trial_separate_main <- trial_survey_multi_choice(
-    preamble = preamble,
+    preamble = preamble_awareness,
     questions = question_multi(
       prompt = insert_variable("prompt"),
       options = form_options,
       name = insert_variable("name"),
       required = TRUE
     ),
-    data = insert_property(stage = "project_choice")
-  )
-
-  if (awareness == "naive") {
-    interstitial_stimulus <- "You will now see one project"
-  } else if (awareness == "aware") {
-    interstitial_stimulus <- insert_javascript(
+    data = insert_property(stage = "project_choice"),
+    on_finish = insert_javascript(
       "function() {
-        interstitial = 'You will now see project ' +
-        jsPsych.data.getLastTrialData().select('current_project_choice_order').values[0] +
-        ' of 20'
-        return interstitial
-        }"
-    )
-  }
-
-  interstitial <-
-    trial_html_button_response(
-      stimulus =interstitial_stimulus,
-      choices = "Continue",
-      data = insert_property(
-        stage = "interstitial"),
-      on_finish = insert_javascript(
-        "function() {
         current_project_choice_order_value = 1 + jsPsych.data.getLastTrialData().select('current_project_choice_order').values[0];
         jsPsych.data.addProperties({
         current_project_choice_order: current_project_choice_order_value
         });
         }"
-      )
     )
+  )
 
   trial_separate_experiment4 <-
-    build_timeline(interstitial, trial_separate_main) %>%
+    build_timeline(trial_separate_main) %>%
     set_variables(prompt = timeline_variable$description,
                   name = timeline_variable$input) %>%
     set_parameters(randomize_order = TRUE) %>%
