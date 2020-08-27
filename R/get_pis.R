95672b973fedd636d58e098eb07cb2f7032d8c39
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-27 14:42:31 +1000

    Remove welcome page from PIS trial
    - After adding the universal welcome page

--- R/get_pis.R
+++ R/get_pis.R
@@ -7,25 +7,22 @@
 ##' @export
 get_pis <- function(sample) {
 
-  welcome <- div(
-    p("Welcome to the study."),
-    p("Make sure to scroll down to the bottom of each page to see the navigation buttons.")) %>%
-    as.character() %>%
-    str_c()
+  img_html <-
+    str_c("pis", 1:3, "_", sample, ".png") %>%
+    map_chr(
+      ~ img(src = insert_resource(.x),
+            width = "750") %>%
+        as.character()
+    )
 
-  img_html <- str_c("pis", 1:3, "_", sample, ".png") %>%
-    map_chr(~ img(src = insert_resource(.x),
-                  width = "750") %>%
-              as.character())
-
-  pis <- trial_instructions(
-    pages = c(
-      welcome,
-      img_html
-    ),
-    show_clickable_nav = TRUE,
-    data = insert_property(stage = "pis")
-  )
+  pis <-
+    trial_instructions(
+      pages = c(
+        img_html
+      ),
+      show_clickable_nav = TRUE,
+      data = insert_property(stage = "pis")
+    )
 
   return(pis)
 
