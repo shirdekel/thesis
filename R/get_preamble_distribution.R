dd16d7e4320b45124afdedb088007e2080dfc228
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-29 14:14:54 +1000

    And change distribution reference
    - Not clear how to make this dynamic

--- R/get_preamble_distribution.R
+++ R/get_preamble_distribution.R
@@ -10,7 +10,7 @@ get_preamble_distribution <- function() {
         p("The numbers on the x-axis (labelled 'Outcome') represent the final amounts of money possible if you chose to invest in all the projects. The numbers on the y-axis (labelled 'Probability') represent the likelihoods of each of the possible outcomes. Negative final outcomes (losses) are shown in red, positive final outcomes (gains) are shown in green, and a final outcome of zero (no loss or gain) is shown in blue.")) %>%
         as.character(),
       div(
-        img(src = insert_resource("distribution.png"),
+        img(src = insert_resource("gambles_plot_experiment2.png"),
             width = "600",
             height = "400"),
         .noWS = "inside"
