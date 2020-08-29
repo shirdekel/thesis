89630221d3c59cc32edf32e7bc05eb6d92242791
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-29 14:22:33 +1000

    Remove col_types specification
    - cols() just mutes it
    
    - Trying out not to specify so as not to get warnings if it doesn't match the specified col_types

--- R/import_data_local.R
+++ R/import_data_local.R
@@ -6,29 +6,11 @@
 ##' @export
 import_data_local <- function(data_directory) {
 
-  col_types <- cols(
-    file_id = col_character(),
-    view_history = col_character(),
-    rt = col_double(),
-    trial_type = col_character(),
-    trial_index = col_double(),
-    time_elapsed = col_double(),
-    internal_node_id = col_character(),
-    experiment = col_character(),
-    sample = col_character(),
-    distribution = col_character(),
-    awareness = col_character(),
-    presentation = col_character(),
-    stimulus = col_character(),
-    button_pressed = col_double(),
-    responses = col_character(),
-    question_order = col_character()
-  )
-
-  data_raw_local <- data_directory %>%
+  data_raw_local <-
+    data_directory %>%
     list.files(full.names = TRUE) %>%
     map_dfr(~ .x %>%
-              read_csv(col_types = col_types))
+              read_csv(col_types = cols()))
 
   return(data_raw_local)
 
