05f9da638f98a5977f209d9dba3e0b56454698a8
Shir Dekel <shirdekel@yahoo.com.au>
2020-08-31 10:27:58 +1000

    Add thesis project and experiment to prolific file name

--- R/get_prolific_id.R
+++ R/get_prolific_id.R
@@ -10,10 +10,20 @@
 ##' @export
 get_prolific_id <- function(data, from_date = "2020-07-28") {
 
+  file_name <-
+    str_c(
+      str_c(
+        "prolific_id",
+        unique(data$thesis_project),
+        unique(data$experiment),
+        sep = "_"),
+      ".txt"
+    )
+
   data %>%
     filter(datetime > from_date) %>%
     pull(prolific) %>%
     unique() %>%
-    write_lines(here("inst", "extdata", "prolific_id.txt"))
+    write_lines(here("inst", "extdata", file_name))
 
 }
