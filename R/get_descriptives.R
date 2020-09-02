e750059d0aabc2c84537183f52a9101debd48b63
Shir Dekel <shirdekel@yahoo.com.au>
2020-09-02 11:50:33 +1000

    Update descriptives
    - Add similarity to groups
    
    - Add sex, total N, and all numerical descriptives (with mean, SD, min, and max) in APA.

--- R/get_descriptives.R
+++ R/get_descriptives.R
@@ -5,18 +5,90 @@
 ##' @export
 get_descriptives <- function(data) {
 
-  allocation <- data %>%
-    nest_by(subject, awareness, presentation, distribution) %>%
+  allocation <-
+    data %>%
+    nest_by(subject, awareness, presentation, distribution, similarity) %>%
     ungroup() %>%
-    count(awareness, presentation, distribution) %>%
+    count(awareness, presentation, distribution, similarity) %>%
     adorn_totals("row")
 
-  time_average <- data %>%
-    pull(total_time) %>%
-    mean()
+  total_apa <-
+    allocation %>%
+    filter(awareness == "Total") %>%
+    pull(n) %>%
+    printnum(numerals = FALSE, capitalize = TRUE)
 
-  descriptives <- list(allocation = allocation,
-                       time_average = time_average)
+  sex <-
+    data %>%
+    nest_by(subject, sex) %>%
+    ungroup() %>%
+    count(sex)
+
+  sex_female <-
+    sex %>%
+    filter(sex == "female") %>%
+    pull(n)
+
+  numerical_names <-
+    c("age", "business_exp", "business_edu", "total_time")
+
+  numerical <-
+    numerical_names %>%
+    map(
+      ~ data %>%
+        summarise(
+          across(
+            all_of(.x),
+            lst(mean, sd, min, max),
+            .names = "{fn}"
+          )
+        ) %>%
+        mutate(across(everything(), ~ .x %>%
+                        printnum(drop0trailing = TRUE)))
+    ) %>%
+    set_names(numerical_names) %>%
+    map(
+      ~str_c(
+        .x$mean,
+        " (*SD* = ",
+        .x$sd,
+        ", *min* = ",
+        .x$min,
+        ", *max* = ",
+        .x$max,
+        ")"
+      )
+    )
+
+  apa <-
+    str_c(
+    total_apa,
+    str_c(
+      "(",
+      sex_female
+    ),
+    "female) people were recruited from the online recruitment platform Prolific. Participants were compensated at a rate of £5 an hour. The average age was",
+    str_c(
+      numerical$age,
+      "."),
+    "Participants reported an average of",
+    numerical$business_exp,
+    "years of work in a business setting, and an average of",
+    numerical$business_edu,
+    "years of business education. The mean completion time was",
+    numerical$total_time,
+    "minutes.",
+    sep = " "
+  )
+
+  descriptives <-
+    lst(
+      allocation,
+      total_apa,
+      sex_female,
+      numerical,
+      apa
+    )
 
   return(descriptives)
 
