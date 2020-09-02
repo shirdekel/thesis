6566027c2d1be6ea41f0f694a2a75c40e57c08d7
Shir Dekel <shirdekel@yahoo.com.au>
2020-09-02 11:56:34 +1000

    Add new static branching grouping values

--- R/get_values.R
+++ R/get_values.R
@@ -22,13 +22,42 @@ get_values <- function() {
   get_screenshots <-
     syms(str_c("get_screenshots", experiment, sep = "_"))
 
+  get_plot <-
+    syms(str_c("get_plot", experiment, sep = "_"))
+
+  get_results <-
+    syms(str_c("get_results", experiment, sep = "_"))
+
+  import_data <-
+    syms(c("import_data_server", "import_data_local" %>%
+             rep(2)))
+
+  data_directory <-
+    c(
+      here("inst", "extdata", "psychsydexp"),
+      here("inst", "jspsych", "experiment3", "data"),
+      here("inst", "jspsych", "experiment4", "data")
+    )
+
+  data_clean_test <-
+    c(
+      FALSE,
+      TRUE %>%
+        rep(2)
+    )
+
   values <-
     lst(
       plot_function,
       gamble_n,
       experiment,
       get_experiment,
-      get_screenshots
+      get_screenshots,
+      get_plot,
+      get_results,
+      import_data,
+      data_directory,
+      data_clean_test
     )
 
   return(values)
