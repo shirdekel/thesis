6b5ffa39926736e1edffa166984c591e75cd6abe
Shir Dekel <shirdekel@yahoo.com.au>
2020-09-21 18:05:27 +1000

    Rename testing directory function
    - And add thesis project specification

---
+++ R/get_testing_directory.R
@@ -0,0 +1,25 @@
+##' @title Get testing directory
+##' @param experiment_number
+##'
+##' @param thesis_project
+##'
+##' @return
+##' @author Shir Dekel
+##' @export
+get_testing_directory <- function(thesis_project, experiment_number) {
+
+  testing_directory <-
+    here(
+      "inst",
+      "jspsych",
+      thesis_project,
+      "testing",
+      str_c(
+        "experiment",
+        experiment_number
+      )
+    )
+
+  return(testing_directory)
+
+}
