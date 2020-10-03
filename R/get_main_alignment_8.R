c3a57ea7688163e9f01edaffefa866620c52ae23
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-03 17:29:49 +1000

    Fix main timeline construction
    - After a lot of fiddling

--- R/get_main_alignment_8.R
+++ R/get_main_alignment_8.R
@@ -8,7 +8,6 @@
 ##' @author Shir Dekel
 ##' @export
 get_main_alignment_8 <- function(gambles, randomize_order = TRUE) {
-
   instructions <-
     get_instructions_alignment_8()
 
@@ -19,10 +18,12 @@ get_main_alignment_8 <- function(gambles, randomize_order = TRUE) {
     projects %>%
     slide(get_project_allocation)
 
+  # Should be a timeline list with length the same as the number of trials to include. If includes conditional timelines, each element of main should be a list of two (one timeline, and conditional)
+
   main <-
-    instructions %>%
-    build_timeline(project_allocation)
+    list(instructions)%>%
+    build_timeline(project_allocation)%>%
+    map(flatten)
 
   return(main)
-
 }
