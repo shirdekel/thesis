27c87ec4a038fe12bdd4d28bf2dca296d68685b0
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-17 12:48:50 +1100

    Update interstitial text
    - The label comes before the checkbox, so it didn't make sense previously

--- R/get_interstitial.R
+++ R/get_interstitial.R
@@ -15,15 +15,14 @@ get_interstitial <- function(interstitial_number) {
   interstitial_html <-
     withTags(
       p(
-        str_c(
-          "You will now see the",
-          ordinal(interstitial_number),
-          "project display. It is important that you pay attention. Click the following checkbox before continuing on to the next page: ",
-          sep = " "
-        ),
         label(
           `for` = interstitial_name,
-          "I will read through and complete the next task carefully."
+          str_c(
+            "You will now see the",
+            ordinal(interstitial_number),
+            "project display. It is important that you pay attention and read through the task carefully. Click the following checkbox before continuing on to the next page: ",
+            sep = " "
+          )
         ),
         input(
           type = "checkbox",
