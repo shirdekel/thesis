fdb12100469fe820756f3d69d521153bb6b92578
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-19 17:05:52 +1100

    Break up long comment

--- R/get_reliability_implicit.R
+++ R/get_reliability_implicit.R
@@ -2,7 +2,8 @@ get_reliability_implicit <- function(npv) {
   range <-
     npv %>%
     map2_chr(
-      c(0.85, 0.05), # low reliability, high reliability, to keep consistent with explicit reliability
+      c(0.85, 0.05), # low reliability, high reliability, to keep consistent
+                     # with explicit reliability
       ~ getrange(.x, .y)
     )
 
