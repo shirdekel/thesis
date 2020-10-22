e83e4bc8979e8062f08b93f7d550c8b52d87e005
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-22 18:04:23 +1100

    Add class argument and style

--- R/get_survey_number.R
+++ R/get_survey_number.R
@@ -1,31 +1,38 @@
 #' Get number-type survey question
 #'
 #'
-#' @param text
+#' @param label_text
 #' @param name
-#' @param max
 #' @param min
-#'
+#' @param max
+#' @param width
+#' @param prefix
+#' @param suffix
+#' @param class
 #' @return
 #' @export
 #'
 #' @examples
-get_survey_number <- function(label_text, name, min = 0, max = 100, width = 70, prefix = "", suffix = "") {
-
+get_survey_number <- function(label_text, name, min = 0, max = 100, width = 70, prefix = "", suffix = "", class = NULL) {
   survey_number <- withTags(
     p(
       p(
-        label(`for` = name,
-            label_text)
-        ),
+        label(
+          `for` = name,
+          label_text
+        )
+      ),
       prefix,
-      input(type = "number",
-            id = name,
-            name = name,
-            min = min,
-            max = max,
-            required = NA,
-            style = str_c("width:", width, "px")),
+      input(
+        type = "number",
+        class = class,
+        id = name,
+        name = name,
+        min = min,
+        max = max,
+        required = NA,
+        style = str_c("width:", width, "px")
+      ),
       suffix
     )
   )
