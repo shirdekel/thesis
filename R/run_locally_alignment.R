f3f59de3015152ef3175a33c6c39c576d7b17357
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-22 18:03:56 +1100

    Add local testing run

--- R/run_locally_alignment.R
+++ R/run_locally_alignment.R
@@ -1,14 +1,29 @@
 ##' @title Run alignment experiment locally
+##'
+##' Couldn't work out how to pass `here` or `file.path` an empty string
 
 ##' @return
 ##' @author Shir Dekel
 ##' @export
-run_locally_alignment <- function(experiment_number) {
-  here(
-    "inst",
-    "jspsych",
-    "alignment",
-    str_c("experiment", (experiment_number))
-  ) %>%
+run_locally_alignment <- function(experiment_number = 8, test = TRUE) {
+  if (test) {
+    path <-
+      here(
+        "inst",
+        "jspsych",
+        "alignment",
+        "testing",
+        str_c("experiment", (experiment_number))
+      )
+  } else {
+    path <-
+      here(
+        "inst",
+        "jspsych",
+        "alignment",
+        str_c("experiment", (experiment_number))
+      )
+  }
+  path %>%
     run_locally()
 }
