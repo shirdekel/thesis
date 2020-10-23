fecbc8aa46484dc7215edb0ebdaae6796a0069db
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-23 14:09:52 +1100

    Update screenshot generation
    - Remove loop, because turns out it's vectorised
    
    - Replace selector with a wider vwidth. Using jspsych-content, or the wide
    argument meant that the alignment 8 tables don't fit the screenshot. Probably
    because we set the width to 150%. So here we're multiplying the default
    width (992) by 150%.
    
    - As a result, all the other screenshots are wider than required, so they are
    now being trimmed at the rmd end.

--- R/get_screenshots.R
+++ R/get_screenshots.R
@@ -8,17 +8,13 @@
 ##' @author Shir Dekel
 ##' @export
 get_screenshots <- function(testing_experiment_directory, screenshot_components) {
-
   if (is_empty(screenshot_components)) return(NA)
 
-  screenshot_components %>%
-    pmap(
-      ~ file.path(testing_experiment_directory, "index.html") %>%
-        webshot(
-          file = .x,
-          selector = "#jspsych-content",
-          eval = .y
-        )
+  file.path(testing_experiment_directory, "index.html") %>%
+    webshot(
+      file = screenshot_components$file_path_materials,
+      eval = screenshot_components$webshot,
+      # to fit alignment 8 tables
+      vwidth = 992 * 1.5
     )
-
 }
