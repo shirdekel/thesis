84681bf2b031b9b0a99600e04b59cfe362bf04ae
Shir Dekel <shirdekel@yahoo.com.au>
2020-10-30 15:52:31 +1100

    Update data simulation
    - Style
    
    - Add ranking (for testing with results target)
    
    - ungroup at the end

--- R/get_data_simulation_alignment.R
+++ R/get_data_simulation_alignment.R
@@ -1,16 +1,18 @@
 ##' @title Data simulation for alignment 8
+##' @param data_clean
 
 ##' @return
 ##' @author Shir Dekel
 ##' @export
 get_data_simulation_alignment <- function(data_clean) {
-
-  data_clean %>%
+  data_simulation_alignment <-
+    data_clean %>%
     nest_by(
       id,
       reliability_amount,
       npv_amount,
       allocation,
+      ranking,
       alignment,
       reliability_type
     ) %>%
@@ -19,8 +21,8 @@ get_data_simulation_alignment <- function(data_clean) {
     mutate(
       allocation = case_when(
         alignment == "high" &
-        reliability_type == "explicit" &
-        reliability_amount == "high" ~ npv_amount %>%
+          reliability_type == "explicit" &
+          reliability_amount == "high" ~ npv_amount %>%
           rand_vect_correlated(
             target_correlation = 1,
             N = 5,
@@ -28,29 +30,31 @@ get_data_simulation_alignment <- function(data_clean) {
             sd = 10
           ),
         alignment == "high" &
-        reliability_type == "explicit" &
-        reliability_amount == "low" ~ npv_amount %>%
-            rand_vect_correlated(
-                target_correlation = -1,
-                N = 5,
-                M = 100,
-                sd = 10
-            ),
+          reliability_type == "explicit" &
+          reliability_amount == "low" ~ npv_amount %>%
+          rand_vect_correlated(
+            target_correlation = -1,
+            N = 5,
+            M = 100,
+            sd = 10
+          ),
         alignment == "low" &
-        reliability_type == "explicit" ~ npv_amount %>%
-            rand_vect_correlated(
-                target_correlation = 1,
-                N = 5,
-                M = 100,
-                sd = 10
-            ),
+          reliability_type == "explicit" ~ npv_amount %>%
+          rand_vect_correlated(
+            target_correlation = 1,
+            N = 5,
+            M = 100,
+            sd = 10
+          ),
         reliability_type == "implicit" ~ npv_amount %>%
-            rand_vect_correlated(
-                target_correlation = 0,
-                N = 5,
-                M = 100,
-                sd = 10
-            )
+          rand_vect_correlated(
+            target_correlation = 0,
+            N = 5,
+            M = 100,
+            sd = 10
+          )
       )
-    )
+    ) %>%
+    ungroup()
+  return(data_simulation_alignment)
 }
