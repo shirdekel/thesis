0889eba11fbe517f2858282b235d8f22a07fa527
Shir Dekel <shirdekel@yahoo.com.au>
2020-12-17 16:22:30 +1100

    Fix total time issue
    - Was calculating on all times
    
    - Use rowwise takes ages
    
    - Also some styling

--- R/clean_data_alignment_8.R
+++ R/clean_data_alignment_8.R
@@ -18,30 +18,38 @@ clean_data_alignment_8 <- function(data_raw_filtered, experiment_number, test, p
 
   data_allocation <-
     data_raw_jspsych_columns_unselected %>%
-    clean_project_allocation()  %>%
+    clean_project_allocation() %>%
     pivot_intrinsic_features() %>%
     pivot_dv()
 
-    data_combined <-
-      data_raw_jspsych_columns_unselected %>%
-      clean_data_other_alignment_8("project_allocation") %>%
-      inner_join(data_allocation, by = "subject")
+  data_combined <-
+    data_raw_jspsych_columns_unselected %>%
+    clean_data_other_alignment_8("project_allocation") %>%
+    inner_join(data_allocation, by = "subject")
 
   data <-
     data_combined %>%
     mutate(
       datetime = dateCreated %>%
         dmy_hms(tz = "Australia/Sydney"),
-      total_time = max(time_elapsed)/60000, # Milliseconds to minutes
+      total_time = time_elapsed %>%
+        map(~ .x %>%
+          max() / 60000), # Milliseconds to minutes
       across(
-        c(reliability_amount,
+        c(
+          reliability_amount,
           reliability_type,
-          alignment),
-        as.factor),
+          alignment
+        ),
+        as.factor
+      ),
       across(where(check_numeric), as.numeric)
     ) %>%
-  select(-c(time_elapsed, dateCreated)) %>%
-    clean_data_finalise_alignment_8(test, prolific_filter, prolific_filter_label)
+    select(-c(time_elapsed, dateCreated)) %>%
+    clean_data_finalise_alignment_8(
+      test, prolific_filter,
+      prolific_filter_label
+    )
 
   return(data)
 }
