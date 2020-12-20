013052a62218ca4e7da65c978ecfa72416ebda32
Shir Dekel <shirdekel@yahoo.com.au>
2020-12-20 17:17:23 +1100

    Add structure reason content

--- R/get_reason_structure.R
+++ R/get_reason_structure.R
@@ -6,30 +6,42 @@
 ##' @author Shir Dekel
 ##' @export
 get_reason_structure <- function() {
-    list(
-        ## high negative
-        c(
-          "centralised_1",
-          "decentralised_1"
-        ) %>%
-     rev(),
-        ## high positive
-        c(
-          "centralised_2",
-          "decentralised_2"
-        ) %>%
-     rev(),
-        ## low negative
-        c(
-          "centralised_3",
-          "decentralised_3"
-        ),
-        ## low positive
-        c(
-          "centralised_4",
-          "decentralised_4"
-        )
+  list(
+    ## high negative
+    c(
+      "key operational decisions were",
+      "communication across relevant business units was"
     ) %>%
-      map(latin_list) %>%
-      c(NA)
+      str_c("delayed with what needed to be a timely process",
+        sep = " "
+      ) %>%
+      rev(),
+    ## high positive
+    c(
+      "there was greater consistency across the individual teams' goals",
+      "the individual teams had greater autonomy to complete their tasks"
+    ) %>%
+      str_c("increasing the efficiency of important project stages",
+        sep = ", "
+      ) %>%
+      rev(),
+    ## low negative
+    c(
+      "poor performers took longer to be replaced",
+      "the teams' project timelines were not coordinated perfectly"
+    ) %>%
+      str_c("so some tasks needed considerable revision",
+        sep = ", "
+      ),
+    ## low positive
+    c(
+      "employees had a clearer chain of command",
+      "employees had a greater sense of initiative"
+    ) %>%
+      str_c("so were able to take care of important challenges early",
+        sep = ", "
+      )
+  ) %>%
+    map(latin_list) %>%
+    c(NA)
 }
