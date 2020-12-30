65997425c0684c60a4a7a9c92189adf9b01077c6
Shir Dekel <shirdekel@yahoo.com.au>
2020-12-30 15:14:07 +1100

    Add interstitials
    - Same as alignment 8

--- R/get_columns_anecdotes_2.R
+++ R/get_columns_anecdotes_2.R
@@ -4,17 +4,18 @@
 ##' @export
 get_columns_anecdotes_2 <- function() {
   columns_anecdotes_2 <-
-      insert_property(
-          subject = insert_javascript("jsPsych.randomization.randomID(15)"),
-          thesis_project = "anecdotes",
-          experiment = "experiment2",
-          sample = "prolific",
-          project_variation = insert_javascript("project_variation_condition"),
-          anecdote_variation = insert_javascript("anecdote_variation_condition"),
-          anecdote_between = insert_javascript("anecdote_between_condition"),
-          PROLIFIC_PID = insert_javascript("urlvar.PROLIFIC_PID"),
-          STUDY_ID = insert_javascript("urlvar.STUDY_ID"),
-          SESSION_ID = insert_javascript("urlvar.SESSION_ID")
-      )
+    insert_property(
+      subject = insert_javascript("jsPsych.randomization.randomID(15)"),
+      thesis_project = "anecdotes",
+      experiment = "experiment2",
+      sample = "prolific",
+      project_variation = insert_javascript("project_variation_condition"),
+      anecdote_variation = insert_javascript("anecdote_variation_condition"),
+      anecdote_between = insert_javascript("anecdote_between_condition"),
+      current_project_display_order = insert_javascript("1"),
+      PROLIFIC_PID = insert_javascript("urlvar.PROLIFIC_PID"),
+      STUDY_ID = insert_javascript("urlvar.STUDY_ID"),
+      SESSION_ID = insert_javascript("urlvar.SESSION_ID")
+    )
   return(columns_anecdotes_2)
 }
--- R/get_projects_anecdotes_2.R
+++ R/get_projects_anecdotes_2.R
@@ -15,14 +15,42 @@ get_projects_anecdotes_2 <- function(project_variation,
                                      anecdote_variation,
                                      anecdote_between,
                                      data) {
-  trial_generic(
-    "survey-html-form3",
-    html = insert_variable("allocation_display"),
-    data = insert_property(stage = "project_allocation")
+  projects <-
+    trial_generic(
+      "survey-html-form3",
+      html = insert_variable("allocation_display"),
+      data = insert_property(stage = "project_allocation")
+  )
+
+  interstitial <-
+      trial_generic(
+          "survey-html-form",
+          html = insert_variable("interstitial"),
+          data = insert_property(stage = "interstitial"),
+          on_finish = insert_javascript(
+              "function() {
+        current_project_display_order_value = 1 + jsPsych.data.getLastTrialData().select('current_project_display_order').values[0];
+        jsPsych.data.addProperties({
+        current_project_display_order: current_project_display_order_value
+        });
+        }"
+          )
+      )
+
+  interstitial_trials <-
+      seq_len(5) %>%
+      map(
+          ~ get_interstitial(.x)
+      )
+
+  build_timeline(
+    interstitial,
+    projects
   ) %>%
     build_timeline() %>%
     set_variables(
-      allocation_display = data$display
+      allocation_display = data$display,
+      interstitial = interstitial_trials
     ) %>%
     set_parameters(randomize_order = FALSE) %>%
     build_timeline() %>%
