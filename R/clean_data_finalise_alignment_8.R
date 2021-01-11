80874d2ee5e200d0f4684e0ee2ff6e12edaffe4e
Shir Dekel <shirdekel@yahoo.com.au>
2021-01-11 12:07:18 +1100

    Update data filtering
    - Need a dataset to approve, which is separate to the dataset to keep for analysis
    
    - Removed 14 participants because they only failed one check, so weren't
    rejected, but the check that they failed was the project_test
    
    - Also add the old alignment export so that this still generates the IDs for the
    old data
    
    - Also, remove the check messages because it is annoying and doesn't seem
    necessary anymore

--- R/clean_data_finalise_alignment_8.R
+++ R/clean_data_finalise_alignment_8.R
@@ -34,42 +34,43 @@ clean_data_finalise_alignment_8 <- function(data, test, prolific_filter,
         ~ data_check %>%
           get_prolific_id_reject(.x, .y)
       )
-    get_check_messages(data_check)
-
-    data_all <-
-      data_check %>%
-      filter(
-        # keep participants that have not been rejected, or those that passed
-        # project test but failed one other test (meaning that no failed project
-        # test participants are kept in the data, even if they only failed just
-        # that check)
-        !reject | (!project_test_fail & check_fail_count == 1)
-      )
-
-    list(prolific_filter, prolific_filter_label) %>%
-      pmap(
-        ~ data_all %>%
-          get_prolific_id(.x, .y)
-      )
 
     # Get IDs of participants approved or awaiting review for subsequent
     # filtering. This helps account for participants that may have finished the
     # experiment, but subsequently returned their submission (or somehow
     # timed-out).
     participants_eligible <-
-      file.path("inst", "extdata", "export",
-                "prolific_export_5fd801122e636e10e21d282c.csv") %>%
-      read_csv(col_types = cols()) %>%
+      file.path("inst", "extdata", "export") %>%
+      list.files(full.names = TRUE) %>%
+      map_dfr(
+        ~ .x %>%
+          read_csv(col_types = cols())
+      ) %>%
       filter(status == "APPROVED" | status == "AWAITING REVIEW") %>%
       pull(participant_id)
 
-    # Remove participants from first batch with faulty input IDs and filter out
-    # ineligible Prolific IDs
-    data <-
-      data_all %>%
+    data_approve <-
+      data_check %>%
       filter(
-        datetime > "2020-12-14",
+        # Keep participants that have not been rejected
+        !reject,
+        # Filter out ineligible Prolific IDs
         prolific %in% participants_eligible
+      )
+
+    list(prolific_filter, prolific_filter_label) %>%
+      pmap(
+        ~ data_approve %>%
+          get_prolific_id(.x, .y)
+      )
+
+    data <-
+      data_approve %>%
+      filter(
+        # Keep participants that passed project test
+        !project_test_fail,
+        # Remove participants from first batch with faulty input IDs
+        datetime > "2020-12-14"
       )
   }
 
