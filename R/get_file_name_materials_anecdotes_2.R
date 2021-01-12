18a5ad32f88f39bcc2ee8c40c9f4fb887f8709c5
Shir Dekel <shirdekel@yahoo.com.au>
2021-01-12 14:32:09 +1100

    Update materials
    - Change - to _ because it's not actually useful for displaying captions

--- R/get_file_name_materials_anecdotes_2.R
+++ R/get_file_name_materials_anecdotes_2.R
@@ -1,6 +1,6 @@
 ##' @title Anecdotes 2 screenshot file names
 ##'
-##' Get all displays from the combined condition, interstitials, and follow-ups,
+##' Get all displays from the combined condition, interstitials, and follow_ups,
 ##' with a sample anecdotes only display at the end.
 ##'
 ##' Don't need to include variations because everyone sees the same domains; the
@@ -13,7 +13,7 @@ get_file_name_materials_anecdotes_2 <- function() {
   project_allocation <-
     c("low", "high") %>%
     map(
-      ~ str_c("project-allocation",
+      ~ str_c("project_allocation",
         "valence",
         c("negative", "positive"),
         "alignment",
@@ -24,8 +24,8 @@ get_file_name_materials_anecdotes_2 <- function() {
     unlist() %>%
     map(
       ~ str_c(.x,
-        "anecdote-condition",
-        c("anecdote-only", "combined"),
+        "anecdote_condition",
+        c("anecdote_only", "combined"),
         sep = "_"
       )
     ) %>%
@@ -33,11 +33,11 @@ get_file_name_materials_anecdotes_2 <- function() {
     simplify_all()
 
   project_allocation[[2]] %>%
-    c("project-allocation_statistics-only") %>%
+    c("project_allocation_statistics_only") %>%
     list(
       str_c("interstitial", seq_len(5), sep = "_"),
       .,
-      str_c("follow-up", seq_len(5), sep = "_")
+      str_c("follow_up", seq_len(5), sep = "_")
     ) %>%
     transpose() %>%
     unlist() %>%
