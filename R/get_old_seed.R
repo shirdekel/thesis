d27bb4c1efa57ac1ced5d35477d14bcd1b38e5bd
Shir Dekel <shirdekel@yahoo.com.au>
2021-01-13 15:15:34 +1100

    Use actual seed values
    - .drake is ignore by git, so when you clone the repo, diagnose doesn't work

--- R/get_old_seed.R
+++ R/get_old_seed.R
@@ -5,14 +5,14 @@
 ##' @export
 get_old_seed <- function() {
 
-  restricted_values <-
-    diagnose(restricted_values)$seed
+  restricted_values <- 2068659205
+    ## diagnose(restricted_values)$seed
 
-  gambles <-
-    diagnose(gambles)$seed
+  gambles <- 336030447
+    ## diagnose(gambles)$seed
 
-  experiment4 <-
-    diagnose(experiment4)$seed
+  experiment4 <- 1827196820
+    ## diagnose(experiment4)$seed
 
   old_seed <-
     lst(
