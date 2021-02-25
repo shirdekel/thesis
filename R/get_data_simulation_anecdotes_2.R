925a18d3be5d0ca181026ba4ee43642a193ccbb5
Shir Dekel <shirdekel@yahoo.com.au>
2021-02-25 11:12:40 +1100

    Update plot
    - Better accounts for statistics only condition by fixing x axis spacing
    
    - Fixes positive valence hypotheses

--- R/get_data_simulation_anecdotes_2.R
+++ R/get_data_simulation_anecdotes_2.R
@@ -5,135 +5,68 @@
 ##' @author Shir Dekel
 ##' @export
 get_data_simulation_anecdotes_2 <- function() {
-  ## loadd(data_clean_anecdotes_1)
+  within <- list(
+    similarity = c("low", "high"),
+    valence = c("negative", "positive")
+  )
 
-  ## anecdote_between <- c("anecdote_only", "combined")
+  between <- list(
+    anecdote = c("anecdote_only", "combined")
+  )
 
-  ## alignment <- c("low", "high")
-
-  ## anecdote_within <- c("statistics_only", "anecdote")
-
-  ## between <- lst(
-  ##   anecdote_between
-  ## )
-
-  ## within <- lst(
-  ##   alignment,
-  ##   anecdote_within
-  ## )
-
-  ## n <- 2
-
-  ## sim_design(
-  ##   within,
-  ##   between,
-  ##   n = n / 2,
-  ##   plot = FALSE,
-  ##   long = TRUE
-  ## ) %>%
-  ##   arrange(id) %>%
-  ##   as_tibble() %>%
-  ##   select(-y) %>%
-  ##   mutate(
-  ##     across(
-  ##       starts_with("anecdote"),
-  ##       ~ .x %>%
-  ##         str_replace("\\.", "_")
-  ##     ),
-  ##     alignment = case_when(
-  ##       anecdote_within == "statistics_only" ~ "NA",
-  ##       TRUE ~ alignment %>% as.character()
-  ##     ),
-  ##     ## valence = case_when(
-  ##     ##   anecdote_within == "statistics_only" ~ "NA",
-  ##     ##   TRUE ~ valence
-  ##     ## )
-  ##   ) %>%
-  ##   distinct()
-
-  ## data_clean_anecdotes_1 %>%
-  ##   lm(
-  ##     allocation_projectA ~ alignment * anecdote,
-  ##     data = .
-  ##   ) %>%
-  ##   y_explicit() <-
-  ##   predict(model_alignment_2, newdata = df)
-
-  ## model.frame() %>%
-  ##   as_tibble() %>%
-  ##   rowid_to_column()
-
-within <- list(
-  similarity = c("low", "high"),
-  valence = c("negative", "positive")
-)
-
-between <- list(
-  anecdote = c("anecdote.only", "combined")
-)
-
-mu <- data.frame(
-  row.names = c("low_negative", "low_positive",
-                "high_negative", "high_positive"),
-  anecdote.only = c(60, 20,
-                    20, 60),
-  combined = c(80, 40,
-               40, 80)
-)
-r <- list(
-  anecdote.only = 0.5,
-  combined = 0.5
-)
-
-df1 <- 
-  sim_design(within, between,
-    n = 100,
-    mu = mu,
-    sd = 10,
-    r = r,
-    dv = "allocation",
-    long = TRUE,
-    plot = FALSE,
-    empirical = TRUE
+  mu <- data.frame(
+    row.names = c(
+      "low_negative", "low_positive",
+      "high_negative", "high_positive"
+    ),
+    anecdote_only = c(
+      60, 40,
+      20, 80
+    ),
+    combined = c(
+      80, 20,
+      40, 60
     )
+  )
+  r <- list(
+    anecdote_only = 0.5,
+    combined = 0.5
+  )
 
-  within_statistics_only <-
-    list(
-      anecdote = "statistics.only",
-  valence = c("negative", "positive")
+  df1 <-
+    sim_design(within, between,
+      n = 100,
+      mu = mu,
+      sd = 10,
+      r = r,
+      dv = "allocation",
+      long = TRUE,
+      plot = FALSE,
+      empirical = TRUE
     )
 
   between_statistics_only <-
     list(
-      anecdote = "anecdote.only"
+      anecdote = "statistics_only"
     )
 
-  mu_statistics_only <-
-      data.frame(
-          row.names = c("statistics.only_negative", "statistics.only_positive"),
-          anecdote.only = c(100, 0)
-      )
-
   df2 <-
-  sim_design(within_statistics_only,
+    sim_design(between_statistics_only,
       n = 100,
-      mu = mu_statistics_only,
+      mu = 80,
       sd = 10,
       r = 0.5,
       dv = "allocation",
       long = TRUE,
       plot = FALSE,
       empirical = TRUE
-  )
+    )
 
   df1 %>%
-    full_join(df2) %>%
+    full_join(df2, by = c("id", "anecdote", "allocation")) %>%
     as_tibble() %>%
     mutate(
       across(similarity, ~ .x %>%
-                           fct_explicit_na(na_level="NA") 
-             )
+        fct_explicit_na(na_level = "NA"))
     )
-
 }
-
--- R/get_plot_simulation_anecdotes_2.R
+++ R/get_plot_simulation_anecdotes_2.R
@@ -11,5 +11,10 @@ get_plot_simulation_anecdotes_2 <- function(data) {
       iv2 = "similarity",
       iv3 = valence,
       dv = "allocation"
+    ) +
+    ## https://stackoverflow.com/a/43903054/13945974
+    facet_grid(~valence,
+        scales = "free_x", space = "free_x",
+        labeller = label_both
     )
 }
